5b2f8fe313b0921b22c911136502789ea4b4be5c
Hideto Yasunori <hideto0118yasunori@gmail.com>
2017-02-21 17:10:32 -0800

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+
+  render() {
+    console.log(this.props);
+    return (
+      <li className="Project">
+      <strong>{this.props.todo.title}</strong>
+      </li>
+    );
+  }
+}
+
+TodoItem.propTypes = {
+  todo: React.PropTypes.object
+}
+
+
+export default TodoItem;
